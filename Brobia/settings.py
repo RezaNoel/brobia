b9216d0f9aecc14fe6b56e953f051f2b9dcf8ba8
RezaNoel <88496269+RezaNoel@users.noreply.github.com>
2023-12-15 18:20:22 +0330

    system files

--- Brobia/settings.py
+++ Brobia/settings.py
@@ -155,7 +155,7 @@ STATICFILES_DIRS = [
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_URL = '/static/'
 
-# STATIC_ROOT = BASE_DIR / "static-cdn"
+# STATIC_ROOT = BASE_DIR / "static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
